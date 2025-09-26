942143364d7d8800a762e1c40759191a7928756a
Temigo <temigo@gmx.com>
2025-09-26 14:50:10 -0700

    Add wavesurfer.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import './App.css';
+import WavesurferPlayer from '@wavesurfer/react';
 
 const App = () => {
+  const [wavesurfer, setWavesurfer] = useState(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const onReady = (ws) => {
+    setWavesurfer(ws)
+    setIsPlaying(false)
+  }
+
+  const onPlayPause = () => {
+    wavesurfer && wavesurfer.playPause()
+  }
+
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <WavesurferPlayer
+        height={100}
+        waveColor="violet"
+        url="./barackobamaunitednations65ARXE.mp3"
+        onReady={onReady}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
+
+      <button onClick={onPlayPause}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
     </div>
   );
 };
